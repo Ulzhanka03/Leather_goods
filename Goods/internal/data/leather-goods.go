eec0904abee79f94e03cd6c98165959cb81020e8
Ulzhanka03 <u_shanbai@kbtu.kz>
2023-10-06 02:38:04 +0600

    Changing keys in the JSON object

--- Goods/internal/data/leather-goods.go
+++ Goods/internal/data/leather-goods.go
@@ -3,13 +3,12 @@ package data
 import "time"
 
 type LeatherGoods struct {
-	ID          int64     // Unique integer ID for the leather good
-	CreatedAt   time.Time // Timestamp for when the leather good is added to our database
-	Name        string    // Name of the leather good
-	Type        string    // Type of the leather good
-	Price       float64   // Price of the leather good
-	LeatherType string    // Type of leather (e.g., natural, eco-friendly)
-	Color       string    // Color of the leather good
-	Version     int32     // The version number starts at 1 and will be incremented each
-	// time the leather good information is updated
+	ID          int64     `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	Name        string    `json:"name"`
+	Type        string    `json:"type"`
+	Price       float64   `json:"price"`
+	LeatherType string    `json:"leather_type"`
+	Color       string    `json:"color"`
+	Version     int32     `json:"version"`
 }
