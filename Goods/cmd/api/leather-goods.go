5e6e8eec8460f8799d8a0a09feafb88948338ee7
Ulzhanka03 <u_shanbai@kbtu.kz>
2023-10-06 02:15:51 +0600

    Update leather-goods.go Encoding Structs

--- Goods/cmd/api/leather-goods.go
+++ Goods/cmd/api/leather-goods.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"goods/Goods/internal/data"
 	"net/http"
+	"time"
 )
 
 func (app *application) createLeatherGoodsHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,5 +16,21 @@ func (app *application) showLeatherGoodsHandler(w http.ResponseWriter, r *http.R
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "show the details of leather good %d\n", id)
+	//fmt.Fprintf(w, "show the details of leather good %d\n", id)
+
+	leatherGood := data.LeatherGoods{
+		ID:          id,
+		CreatedAt:   time.Now(),
+		Name:        "Handcrafted Leather Bag",
+		Type:        "Portfolio",
+		Price:       99.99,
+		LeatherType: "Natural",
+		Color:       "Brown",
+		Version:     1,
+	}
+	err = app.writeJSON(w, http.StatusOK, leatherGood, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
