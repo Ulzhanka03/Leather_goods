bbe4491ee8a3f14e918f43d7aaae71a4a3b9df35
Ulzhanka03 <u_shanbai@kbtu.kz>
2023-10-05 23:59:33 +0600

    readIDparam

--- Goods/cmd/api/leather-goods.go
+++ Goods/cmd/api/leather-goods.go
@@ -2,23 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"strconv"
 )
 
 func (app *application) createLeatherGoodsHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "create a new leather good")
 }
 func (app *application) showLeatherGoodsHandler(w http.ResponseWriter, r *http.Request) {
-
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
-
 	fmt.Fprintf(w, "show the details of leather good %d\n", id)
 }
