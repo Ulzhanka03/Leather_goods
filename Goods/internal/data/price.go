45f5c4adb132f155f3cbed011fdc914290db4456
Ulzhanka03 <u_shanbai@kbtu.kz>
2023-10-07 00:31:16 +0600

    Chapter 4.4. The json.Unmarshaler interface

--- Goods/internal/data/price.go
+++ Goods/internal/data/price.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidPriceFormat = errors.New("invalid price format")
 
 type Price float64
 
@@ -14,4 +18,25 @@ func (p Price) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+func (p *Price) UnmarshalJSON(jsonValue []byte) error {
+
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidPriceFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "$" {
+		return ErrInvalidPriceFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidPriceFormat
+	}
+
+	*p = Price(i)
+	return nil
 }
