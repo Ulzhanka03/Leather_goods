647c5f808ae99b15a59e27bc73790984b2a9c0d8
Ulzhanka03 <u_shanbai@kbtu.kz>
2023-10-07 00:01:10 +0600

    Chapter 4.2. Update leather-goods.go

--- Goods/cmd/api/leather-goods.go
+++ Goods/cmd/api/leather-goods.go
@@ -17,7 +17,7 @@ func (app *application) createLeatherGoodsHandler(w http.ResponseWriter, r *http
 	}
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 	fmt.Fprintf(w, "%+v\n", input)
