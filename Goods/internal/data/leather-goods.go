5c10254b9f008deb1b0cbb677f59ee89b4259357
Ulzhanka03 <u_shanbai@kbtu.kz>
2023-10-06 22:31:14 +0600

    Chapter 3.5 Embedding an alias

--- Goods/internal/data/leather-goods.go
+++ Goods/internal/data/leather-goods.go
@@ -1,14 +1,36 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
 
 type LeatherGoods struct {
 	ID          int64     `json:"id"`
 	CreatedAt   time.Time `json:"-"`
 	Name        string    `json:"name"`
 	Type        string    `json:"type"`
-	Price       Price     `json:"price"`
+	Price       float64   `json:"price"`
 	LeatherType string    `json:"leather_type"`
 	Color       string    `json:"color"`
 	Version     int32     `json:"version"`
+}
+
+func (l LeatherGoods) MarshalJSON() ([]byte, error) {
+	var price string
+	if l.Price != 0 {
+		price = fmt.Sprintf("%.2f $", l.Price)
+	}
+
+	type LeatherGoodsAlias LeatherGoods
+
+	aux := struct {
+		LeatherGoodsAlias
+		Price string `json:"price"`
+	}{
+		LeatherGoodsAlias: LeatherGoodsAlias(l),
+		Price:             price,
+	}
+	return json.Marshal(aux)
 }
