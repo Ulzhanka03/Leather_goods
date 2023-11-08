b78e9c487258205a78f9a1c846fbb3a0da1d4d07
Ulzhanka03 <u_shanbai@kbtu.kz>
2023-11-08 19:58:24 +0600

    Chapter 13.3. Registering a User

--- Goods/cmd/api/routes.go
+++ Goods/cmd/api/routes.go
@@ -18,6 +18,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/leather-goods/:id", app.showLeatherGoodsHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/leather-goods/:id", app.updateLeatherGoodsHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/leather-goods/:id", app.deleteLeatherGoodsHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	return app.recoverPanic(app.rateLimit(router))
 
 }
