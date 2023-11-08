a51f295c0fe342d1673145528853fde7407403f7
Ulzhanka03 <u_shanbai@kbtu.kz>
2023-11-08 21:38:03 +0600

    Chapter 14.3. Sending a Welcome Email

--- Goods/cmd/api/main.go
+++ Goods/cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/lib/pq"
 	"goods/Goods/internal/data"
 	"goods/Goods/internal/jsonlog"
+	"goods/Goods/internal/mailer"
 	"os"
 	"time"
 )
@@ -28,12 +29,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -52,6 +61,11 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "547fb6615c1825", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "aee1c42398dcda", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Goods <no-reply@goods.alexedwards.net>", "SMTP sender")
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
@@ -68,6 +82,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
