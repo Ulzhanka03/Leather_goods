ea445985e99d749a0eea984a5ca48cb3cc501214
Ulzhanka03 <u_shanbai@kbtu.kz>
2023-11-10 22:36:32 +0600

    Chapter 17.1. Requiring User Activation

--- Goods/cmd/api/routes.go
+++ Goods/cmd/api/routes.go
@@ -13,11 +13,12 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/leather-goods", app.listLeatherGoodsHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/leather-goods", app.createLeatherGoodsHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/leather-goods/:id", app.showLeatherGoodsHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/leather-goods/:id", app.updateLeatherGoodsHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/leather-goods/:id", app.deleteLeatherGoodsHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/leather-goods", app.requireActivatedUser(app.listLeatherGoodsHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/leather-goods", app.requireActivatedUser(app.createLeatherGoodsHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/leather-goods/:id", app.requireActivatedUser(app.showLeatherGoodsHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/leather-goods/:id", app.requireActivatedUser(app.updateLeatherGoodsHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/leather-goods/:id", app.requireActivatedUser(app.deleteLeatherGoodsHandler))
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
