3386d1525893152db579c41058fbb423a4d02ccd
Ulzhanka03 <u_shanbai@kbtu.kz>
2023-11-10 20:27:18 +0600

    Chapter 14.4. Sending Background Emails

--- Goods/cmd/api/users.go
+++ Goods/cmd/api/users.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"goods/Goods/internal/data"
 	"goods/Goods/internal/validator"
 	"net/http"
@@ -22,10 +23,8 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 	}
 
 	user := &data.User{
-		Name:      input.Name,
-		Email:     input.Email,
-		Activated: false,
-	}
+		Name:  input.Name,
+		Email: input.Email}
 
 	err = user.Password.Set(input.Password)
 	if err != nil {
@@ -51,12 +50,21 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		}
 		return
 	}
-	err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				app.logger.PrintError(fmt.Errorf("%s", err), nil)
+			}
+		}()
+		app.background(func() {
+			err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+			if err != nil {
+				app.logger.PrintError(err, nil)
+			}
+		})
+	}()
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
