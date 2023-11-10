4127898914616b70b1aa384ee485a8c9fbb2d84b
Ulzhanka03 <u_shanbai@kbtu.kz>
2023-11-10 22:46:15 +0600

    Chapter 17.3. Setting up the Permissions Model

--- Goods/internal/data/models.go
+++ Goods/internal/data/models.go
@@ -12,6 +12,7 @@ var (
 
 type Models struct {
 	LeatherGoods LeatherGoodsModel
+	Permissions  PermissionModel
 	Tokens       TokenModel
 	Users        UserModel
 }
@@ -19,6 +20,7 @@ type Models struct {
 func NewModels(db *sql.DB) Models {
 	return Models{
 		LeatherGoods: LeatherGoodsModel{DB: db},
+		Permissions:  PermissionModel{DB: db},
 		Tokens:       TokenModel{DB: db},
 		Users:        UserModel{DB: db},
 	}
