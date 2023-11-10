3db9ca10698c6b1d5dc363f87f0703391f20596f
Ulzhanka03 <u_shanbai@kbtu.kz>
2023-11-10 21:08:25 +0600

    Chapter 15.3. Sending Activation Tokens

--- Goods/cmd/api/users.go
+++ Goods/cmd/api/users.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"goods/Goods/internal/data"
 	"goods/Goods/internal/validator"
 	"net/http"
+	"time"
 )
 
 func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -50,20 +50,24 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		}
 		return
 	}
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 
-	go func() {
-		defer func() {
-			if err := recover(); err != nil {
-				app.logger.PrintError(fmt.Errorf("%s", err), nil)
-			}
-		}()
-		app.background(func() {
-			err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
-			if err != nil {
-				app.logger.PrintError(err, nil)
-			}
-		})
-	}()
+	app.background(func() {
+
+		data := map[string]interface{}{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
+		if err != nil {
+			app.logger.PrintError(err, nil)
+		}
+	})
 	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
