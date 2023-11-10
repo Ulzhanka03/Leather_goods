185b1255324dacdb1292775ffd7c2e88ed8bedab
Ulzhanka03 <u_shanbai@kbtu.kz>
2023-11-10 23:03:47 +0600

    Chapter 17.4. Checking Permissions

--- Goods/cmd/api/routes.go
+++ Goods/cmd/api/routes.go
@@ -14,11 +14,11 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
-	router.HandlerFunc(http.MethodGet, "/v1/leather-goods", app.requireActivatedUser(app.listLeatherGoodsHandler))
-	router.HandlerFunc(http.MethodPost, "/v1/leather-goods", app.requireActivatedUser(app.createLeatherGoodsHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/leather-goods/:id", app.requireActivatedUser(app.showLeatherGoodsHandler))
-	router.HandlerFunc(http.MethodPatch, "/v1/leather-goods/:id", app.requireActivatedUser(app.updateLeatherGoodsHandler))
-	router.HandlerFunc(http.MethodDelete, "/v1/leather-goods/:id", app.requireActivatedUser(app.deleteLeatherGoodsHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/leather-goods", app.requirePermission("leather-goods:read", app.listLeatherGoodsHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/leather-goods", app.requirePermission("leather-goods:write", app.createLeatherGoodsHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/leather-goods/:id", app.requirePermission("leather-goods:read", app.showLeatherGoodsHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/leather-goods/:id", app.requirePermission("leather-goods:write", app.updateLeatherGoodsHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/leather-goods/:id", app.requirePermission("leather-goods:write", app.deleteLeatherGoodsHandler))
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
